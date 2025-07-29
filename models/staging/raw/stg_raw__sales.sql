f3257eae969d6584a8b06f62b5d36154efdb3fce
dawson.jerome19@gmail.com <dawson.jerome19@gmail.com>
2025-07-29 14:09:09 +0000

    pitié

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -9,7 +9,7 @@ source as (
 renamed as (
 
     select
-        concat(products_id,"_",orders_id),
+        concat(products_id,"-",orders_id) as primary_key,
         date_date,
         orders_id,
         pdt_id as products_id,
