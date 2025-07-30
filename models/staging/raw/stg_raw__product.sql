ff9d9935865bd4eefb7cf03fab557d281bc328fa
dawson.jerome19@gmail.com <dawson.jerome19@gmail.com>
2025-07-30 09:44:55 +0000

    er

--- models/staging/raw/stg_raw__product.sql
+++ models/staging/raw/stg_raw__product.sql
@@ -1,19 +1,12 @@
-with 
-
+with
 source as (
-
     select * from {{ source('raw', 'product') }}
-
 ),
-
 renamed as (
-
     select
         products_id,
-        CAST(purchse_price AS FLOAT64) Spurchase_price
-
+        CAST(purchse_price AS FLOAT64) AS purchased_price
     from source
-
 )
 
 select * from renamed
