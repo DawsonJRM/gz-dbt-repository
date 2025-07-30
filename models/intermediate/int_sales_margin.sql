72f0bcc510f436c393b4b101b954a5f580264a91
dawson.jerome19@gmail.com <dawson.jerome19@gmail.com>
2025-07-30 10:03:50 +0000

    654

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -5,7 +5,7 @@
       orders_id,
       revenue,
       quantity,
-      purchase_price,
+      purchased_price,
       ROUND(s.quantity*p.purchased_price,2) AS purchase_cost,
       ROUND(s.revenue - s.quantity*p.purchased_price, 2) AS margin
   FROM {{ref("stg_raw__sales")}} s
